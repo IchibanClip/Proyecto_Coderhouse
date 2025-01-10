ac92a5c7c529aa7ab91de2c639de7dff4d4f1a11
Ichiban <Iraiza2703@gmail.com>
2025-01-09 23:54:28 -0500

    mejora del settings y urls

--- ProyectoTienda/urls.py
+++ ProyectoTienda/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from mymusicstore import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("TiendaVinilos/", views.tienda_vinilos, name="tienda_vinilos"),
+    path("", include("Main.urls")),
+    path("catalogo/", include("mymusicstore.urls")),
 ]
